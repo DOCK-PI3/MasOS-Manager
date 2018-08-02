9414907adb29837396ce2c500fe13403ed957b83
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2018-08-02 01:08:32 +0100

    D
    D

--- __init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Django project to manage a Recalbox from a web interface
-"""
-__version__ = '1.1.1'
--- project/manager_frontend/views/__init__.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-
-class HomeView(TemplateView):
-    """
-    Homepage view is actually only a "direct render to template"
-    """
-    template_name = "manager_frontend/home.html"
- 
