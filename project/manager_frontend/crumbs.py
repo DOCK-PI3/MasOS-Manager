271d65898391ab76a1a57764afaf5feea5028172
Moriggy <41973660+Moriggy@users.noreply.github.com>
2018-08-04 18:45:06 +0200

    update
    update

--- project/manager_frontend/crumbs.py
+++ project/manager_frontend/crumbs.py
@@ -6,14 +6,14 @@ from autobreadcrumbs import site
 from django.utils.translation import ugettext_lazy
 
 site.update({
-    'manager:home': ugettext_lazy('Home'),
+    'manager:home': ugettext_lazy('Inicio'),
     'manager:bios': ugettext_lazy('Bios'),
-	'manager:configes': ugettext_lazy('ES Configuration'),
-    'manager:config': ugettext_lazy('RetroArch Configuration'),
+	'manager:configes': ugettext_lazy('Configuratión de ES'),
+    'manager:config': ugettext_lazy('Configuratión de RetroArch'),
 	'manager:configas': ugettext_lazy('Autostart Script'),
     'manager:logs': ugettext_lazy('Logs'),
-    'manager:monitoring': ugettext_lazy('Monitoring'),
-    'manager:roms-systems': ugettext_lazy('Rom by system'),
+    'manager:monitoring': ugettext_lazy('Monitorización'),
+    'manager:roms-systems': ugettext_lazy('Rom por sistema'),
     'manager:roms-list': ugettext_lazy('{{ system_name }}'),
     #'manager:roms-saves-list': ugettext_lazy('Saves'),
 })
