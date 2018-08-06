dd071574f467a01d4acdd185da36ca8a505dd7fd
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2018-08-06 14:19:10 +0100

    text update
    text

--- rpmanager.sh
+++ rpmanager.sh
@@ -2,21 +2,21 @@
 # rpmanager.sh
 ###############
 #
-# The RetroPie-Manager's manager.
+# The MasOS-Manager's manager.
 #
-# This script MUST be in the same directory RetroPie-Manager's directory.
-# If it's installed in "/opt/retropie/supplementary/retropie-manager/",
+# This script MUST be in the same directory MasOS-Manager's directory.
+# If it's installed in "/opt/masos/supplementary/retropie-manager/",
 # and only in this case, the user can make symbolic links to it. Otherwise,
 # the symbolic links won't work.
 #
-# The RetroPie-Manager can not be started directly by the root user, but
+# The MasOS-Manager can not be started directly by the root user, but
 # but if you need it (ex.: on boot), use the --user option. The user must
-# be a RetroPie user (must have a RetroPie directory tree in its homedir).
+# be a MasOS user (must have a MasOS directory tree in its homedir).
 # If it's called in a "sudo environment", it's OK, the sudo user will start
 # the service.
 #
 # If the --log option is used, the log messages will be saved in
-# "/opt/retropie/supplementary/retropie-manager/logs" ($rpmanager_dir/logs)
+# "/opt/masos/supplementary/retropie-manager/logs" ($rpmanager_dir/logs)
 # with an appropriate file name
 #
 # Execute it with --help to see the available options.
@@ -34,19 +34,19 @@ The OPTIONS are:
 
 -h|--help           print this message and exit
 
---start             start the RetroPie-Manager
+--start             start the MasOS-Manager
 
---stop              stop the RetroPie-Manager
+--stop              stop the MasOS-Manager
 
---isrunning         show if RetroPie-Manager is running and the
+--isrunning         show if MasOS-Manager is running and the
                     listening port and exit
 
 --log               save the log messages (optional, default: not save log
                     messages, only works with --start)
 
--u|--user USER      start RetroPie-Manager as USER (only available for
+-u|--user USER      start MasOS-Manager as USER (only available for
                     privileged users, only works with --start, USER must 
-                    be a RetroPie user)
+                    be a MasOS user)
 
 The --start and --stop options are, obviously, mutually exclusive. If the
 user uses both, only the first works."
@@ -64,14 +64,14 @@ user="$SUDO_USER"
 
 
 ##############################################################################
-# Set the user that will start the RetroPie-Manager. Only privileged users
-# can use this function. The user MUST have a RetroPie directory tree in
+# Set the user that will start the MasOS-Manager. Only privileged users
+# can use this function. The user MUST have a MasOS directory tree in
 # its homedir.
 #
 # Globals:
 #   user
 # Arguments:
-#   $1  a valid RetroPie user name
+#   $1  a valid MasOS user name
 # Returns:
 #   0, if a the user is setted correctly
 #   non-zero, otherwise
@@ -83,7 +83,7 @@ function set_user() {
     fi
 
     if [[ ! -d "/home/$1/MasOS/" ]]; then
-        echo "Error: the user '$1' is not a RetroPie user." >&2
+        echo "Error: the user '$1' is not a MasOS user." >&2
         return 1
     fi
 
@@ -93,7 +93,7 @@ function set_user() {
 
 
 ##############################################################################
-# Check if RetroPie-Manager is running. If positive, fill the $port global
+# Check if MasOS-Manager is running. If positive, fill the $port global
 # variable with current listening port.
 #
 # Globals:
@@ -101,7 +101,7 @@ function set_user() {
 # Arguments:
 #   None
 # Returns:
-#   0, if RetroPie-Manager is running
+#   0, if MasOS-Manager is running
 #   non-zero, otherwise
 ##############################################################################
 function is_running() { 
@@ -127,9 +127,9 @@ function is_running() {
 
 
 ##############################################################################
-# Starts the RetroPie-Manager service. It gives an error if it's called
+# Starts the MasOS-Manager service. It gives an error if it's called
 # directly by the root user, but if it's called by an "sudo environment",
-# RetroPie-Manager is started by the sudo user.
+# MasOS-Manager is started by the sudo user.
 #
 # Globals:
 #   log_command
@@ -137,12 +137,12 @@ function is_running() {
 # Arguments:
 #   None
 # Returns:
-#   1, if it's unable to start RetroPie-Manager
-#   0, if RetroPie-Manager is successfully started
+#   1, if it's unable to start MasOS-Manager
+#   0, if MasOS-Manager is successfully started
 ##############################################################################
 function start_service() {
     if is_running; then
-        echo "Nothing done. RetroPie-Manager is already running and listening at $port." >&2
+        echo "Nothing done. MasOS-Manager is already running and listening at $port." >&2
         return 1
     fi
 
@@ -153,58 +153,58 @@ function start_service() {
                       --noreload \
                       $log_command"
 
-    # RetroPie-Manager should not be started directly by the root user,
+    # MasOS-Manager should not be started directly by the root user,
     # but we can deal if it's called by a "sudo environment" or with
     # the --user option.
     if [[ $(id -u) -eq 0 ]]; then
         if [[ $(id -u "$user") -eq 0 ]]; then
-            echo "Error: RetroPie-Manager can't be started directly by root!" >&2
+            echo "Error: MasOS-Manager can't be started directly by root!" >&2
             echo "Try to use '--user' option" >&2
             return 1
         fi
         startcmd="su -c '$startcmd' $user"
     fi
 
-    echo "Starting RetroPie-Manager..."
+    echo "Starting MasOS-Manager..."
     eval $startcmd &>/dev/null &
     sleep 3
     if is_running; then
-        echo "RetroPie-Manager is running and listening at port $port"
+        echo "MasOS-Manager is running and listening at port $port"
         return 0
     else
-        echo "Error: It seems that RetroPie-Manager had some problem to start!" >&2
+        echo "Error: It seems that MasOS-Manager had some problem to start!" >&2
         return 1
     fi
 }
 
 
 ##############################################################################
-# Stops the RetroPie-Manager service if it's running.
+# Stops the MasOS-Manager service if it's running.
 #
 # Globals:
 #   None
 # Arguments:
 #   None
 # Returns:
-#   0, if RetroPie-Manager process is successfully killed
-#   1, if unable to kill RetroPie-Manager
-#   2, if RetroPie-Manager wasn't running from the start
+#   0, if MasOS-Manager process is successfully killed
+#   1, if unable to kill MasOS-Manager
+#   2, if MasOS-Manager wasn't running from the start
 ##############################################################################
 function stop_service() {
     if is_running; then
-        echo "Stopping RetroPie-Manager..."
+        echo "Stopping MasOS-Manager..."
         sudo kill -9 $(pgrep -f 'python.*manage\.py.*runserver')
         sleep 1
         if is_running; then
-            echo "Error: Unable to kill RetroPie-Manager process." >&2
+            echo "Error: Unable to kill MasOS-Manager process." >&2
             return 1
         else
-            echo "RetroPie-Manager has been stopped."
+            echo "MasOS-Manager has been stopped."
             return 0
         fi
     fi
 
-    echo "Nothing done. RetroPie-Manager wasn't running." >&2
+    echo "Nothing done. MasOS-Manager wasn't running." >&2
     return 2
 }
 
@@ -215,7 +215,7 @@ function stop_service() {
 if [ ! -x "$rpmanager_dir/bin/python" -a ! -f "$rpmanager_dir/manage.py" ]; then
     rpmanager_dir="/opt/masos/supplementary/retropie-manager"
     if [[ ! -d "$rpmanager_dir" ]]; then
-        echo "Error: $(basename $0) MUST be in the RetroPie-Manager's directory" >&2
+        echo "Error: $(basename $0) MUST be in the MasOS-Manager's directory" >&2
         exit 1
     fi
 fi
@@ -243,10 +243,10 @@ while [[ "$1" ]]; do
 
     --isrunning)
         if is_running; then
-            echo "RetroPie-Manager is running and listening at port $port"
+            echo "MasOS-Manager is running and listening at port $port"
             exit 0
         else
-            echo "RetroPie-Manager is not running"
+            echo "MasOS-Manager is not running"
             exit 1
         fi
     ;;
